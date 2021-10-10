0df1031311c21eae9264dc8511b64387bf5d3182
Raffique Muir <raffiquemuir@yahoo.com>
2021-10-10 14:23:21 -0500

    Added more files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,8 @@
- 
+window.onload = () => {
+    var board = document.getElementById('board').children
+    for(let i = 0; i<9; i++){
+        board[i].id = 'box'+String(i)
+        board[i].classList.toggle('square')
+    }
+
+}
